4694d9b884cb79bcce6105250510842348eef57d
Franco Micheletti <fran_mich@live.com>
2025-06-14 12:48:18 -0300

    css fix added url video

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,22 +1,22 @@
 import { Urls } from "./urls"
 
-export const Project = ({projectName,projectInfo,projectImage,githubUrl,websiteUrl,videoUrl,technologies}) => {
+export const Project = ({ projectName, projectInfo, projectImage, githubUrl, websiteUrl, videoUrl, technologies }) => {
 
-    return (
-        <div className="project select-none rounded-xl justify-center flex items-center flex-col shadow-2xl">
-            <div>
-                <img className="border-b-2 project-image rounded-tr-md rounded-tl-md" width={320} height={160} src={projectImage}></img>
-            </div>
-            <div className="bg-white flex h-20 justify-center items-center text-lg w-full flex-col">
-                <div className="font-bold text-black">{projectName}</div>
-                <div className="font-bold text-gray-500">{technologies}</div>
-            </div>
-            
-            <Urls 
-                githubUrl={githubUrl}
-                websiteUrl={websiteUrl}
-                videoUrl={videoUrl}
-            />
-        </div>
-    )
+  return (
+    <div className="project w-[300px] select-none rounded-xl justify-start flex items-start flex-col shadow-2xl">
+      <div>
+        <img className="border-b-2 project-image rounded-tr-md rounded-tl-md" width={320} height={160} src={projectImage}></img>
+      </div>
+      <div className="bg-white flex justify-center items-center text-lg w-full flex-col p-4 h-[120px]">
+        <div className="font-bold text-black">{projectName}</div>
+        <div className="font-bold text-gray-500">{technologies}</div>
+      </div>
+
+      <Urls
+        githubUrl={githubUrl}
+        websiteUrl={websiteUrl}
+        videoUrl={videoUrl}
+      />
+    </div>
+  )
 }
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -20,42 +20,42 @@ export const Projects = () => {
       <div className="bg-white w-auto min-h-screen">
         <div style={{ backgroundColor: "rgb(211, 211, 211,0.30)" }} className="min-h-screen justify-center flex flex-col items-center p-5">
           <div className="projects-title text-8xl text-center text-sky-900 font-semibold p-5 mt-12 select-none">PROJECTS</div>
-          <div className="flex flex-col mt-10 p-3 project-type-frontend">
-
-            <div className="project-type-title text-8xl text-white font-semibold flex select-none">Frontend</div>
-            <div className="projects-list inline-flex align-baseline mt-20 gap-5">
-              {/* E commerce */}
-              <Project projectName={"E-commerce Frontend"}
-                projectImage={"./images/ecommerce-main.webp"}
-                githubUrl={"https://github.com/Franco-Micheletti/ecommerce-frontend"}
-                videoUrl={"https://drive.google.com/uc?id=1Qh4WuMbJ6H9b2D3zIV9ODM0YDdYgsaNL"}
-                technologies={"React | Redux"} />
-              {/* International Weather App*/}
-              <Project projectName={"International Weather App"}
-                projectImage={"./images/weather-main.webp"}
-                githubUrl={"https://github.com/Franco-Micheletti/international-weather-app/tree/main/weather-app"}
-                websiteUrl={"https://international-weather-app.vercel.app/"}
-                technologies={"React | Tailwind | Vite.js"}
-              />
-              {/* AoA APP*/}
-              <Project projectName={"Aoe2 DE Analytics"}
-                projectImage={"./images/aoa.webp"}
-                githubUrl={"https://github.com/Franco-Micheletti/AoA"}
-                websiteUrl={"https://aoa.vercel.app/"}
-                technologies={"React | Next.js | Tailwind"}
-              />
-              {/* URL SHORTENER*/}
-              <Project projectName={"URL Shortener"}
-                projectImage={"./images/url-shortener.webp"}
-                githubUrl={"https://github.com/Franco-Micheletti/url-shortener-frontend"}
-                websiteUrl={"https://myshorturls.vercel.app/"}
-                technologies={"React | Tailwind | Vite.js"}
-              />
+          <div className="flex flex-col mt-10 p-3 project-type-frontend justify-center items-center">
+            <div className="project-type-title text-8xl text-white font-semibold flex select-none py-4">Frontend</div>
+            <div className="w-full flex justify-center items-center">
+              <div className="projects-list w-fit flex-wrap inline-flex justify-center content-start items-start mt-20 gap-5">
+                {/* E commerce */}
+                <Project projectName={"E-commerce Frontend"}
+                  projectImage={"./images/ecommerce-main.webp"}
+                  githubUrl={"https://github.com/Franco-Micheletti/ecommerce-frontend"}
+                  videoUrl={"https://drive.google.com/file/d/1Qh4WuMbJ6H9b2D3zIV9ODM0YDdYgsaNL/view"}
+                  technologies={"React | Redux"} />
+                {/* International Weather App*/}
+                <Project projectName={"International Weather App"}
+                  projectImage={"./images/weather-main.webp"}
+                  githubUrl={"https://github.com/Franco-Micheletti/international-weather-app/tree/main/weather-app"}
+                  websiteUrl={"https://international-weather-app.vercel.app/"}
+                  technologies={"React | Tailwind | Vite.js"}
+                />
+                {/* AoA APP*/}
+                <Project projectName={"Aoe2 Analytics"}
+                  projectImage={"./images/aoa.webp"}
+                  websiteUrl={"https://aoa.vercel.app/"}
+                  technologies={"React | Next.js | Tailwind"}
+                />
+                {/* URL SHORTENER*/}
+                <Project projectName={"URL Shortener"}
+                  projectImage={"./images/url-shortener.webp"}
+                  githubUrl={"https://github.com/Franco-Micheletti/url-shortener-frontend"}
+                  websiteUrl={"https://myshorturls.vercel.app/"}
+                  technologies={"React | Tailwind | Vite.js"}
+                />
+              </div>
             </div>
           </div>
-          <div className="flex flex-col mt-10 p-3 mb-20 project-type-backend">
-            <div className="project-type-title text-8xl text-white font-semibold flex select-none">Backend</div>
-            <div className="projects-list inline-flex align-baseline mt-20 gap-5">
+          <div className="flex flex-col mt-10 p-3 mb-20 project-type-backend justify-center items-center">
+            <div className="project-type-title text-8xl text-white font-semibold flex select-none py-4">Backend</div>
+            <div className="projects-list w-fit flex-wrap inline-flex justify-center content-start items-start mt-20 gap-5">
               {/* E-commerce Backend*/}
               <Project projectName={"E-commerce Backend"}
                 projectImage={"./images/e-commerce-backend.webp"}
@@ -65,11 +65,12 @@ export const Projects = () => {
               <Project projectName={"URL Shortener Backend"}
                 projectImage={"./images/url-shortener-backend-image.webp"}
                 githubUrl={"https://github.com/Franco-Micheletti/url-shortener-backend/"}
-                technologies={"Python - Django REST Framework - mySQL"} />
+                technologies={"Python | Django Rest | mySQL | JWT"} />
               {/* Vehicles API */}
               <Project projectName={"Vehicles API"}
                 projectImage={"./images/car-api.webp"}
-                githubUrl={"https://github.com/Franco-Micheletti/test-dev"} />
+                githubUrl={"https://github.com/Franco-Micheletti/test-dev"}
+                technologies={"Python | Django Rest | SQLite"} />
               {/* Frequency Table*/}
               <Project projectName={"Frequency table Program"}
                 projectImage={"./images/frequency-table.webp"}
